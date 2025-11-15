7a30d40f36926b5d74e22d09740204b192275940
spynevan <spynevan007@gmail.com>
2025-11-15 10:24:45 -0600

    added num of words

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def get_book_text(file_path):
         file_content = file.read()
         return file_content
 
-def main():
+def get_num_of_words(string_input):
+    string_list = string_input.split()
+    return len(string_list)
 
+def main():
     file_path = 'books/frankenstein.txt'
     data = get_book_text(file_path)
-    print(data)
+    num_words = get_num_of_words(data)
+    print(f"Found {num_words} total words")
     
 main()
