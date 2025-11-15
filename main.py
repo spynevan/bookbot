45fa839f79ff94f68b525aa4206e80c53ac3121d
spynevan <spynevan007@gmail.com>
2025-11-15 10:46:18 -0600

    added a get_word_dictionary function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import get_num_words
-from stats import get_book_text 
+from stats import *
 
 def main():
     file_path = 'books/frankenstein.txt'
     data = get_book_text(file_path)
     num_words = get_num_words(data)
     print(f"Found {num_words} total words")
+    word_dict = get_word_dictionary(data)
+    print(word_dict)
+
     
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,12 @@ def get_book_text(file_path):
 def get_num_words(string_input):
     string_list = string_input.split()
     return len(string_list)
+
+def get_word_dictionary(string_input):
+    return_object = dict()
+    for character in string_input.lower():
+        if character not in return_object:
+            return_object[character] = 0
+        return_object[character] += 1
+    return return_object
+
